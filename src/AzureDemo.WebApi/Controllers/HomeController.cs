1fe9376bb136fdd260f74257adb37439a7983f50
Alex Watts <alexjwatts83@gmail.com>
2021-08-25 07:35:29 +1000

    Added home controller

---
+++ src/AzureDemo.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Google.Apis.Auth.AspNetCore3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDemo.WebApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return Ok("Home");
+		}
+	}
+	[ApiController]
+	[Route("[controller]")]
+	public class IndexController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return Ok("Index");
+		}
+	}
+}
