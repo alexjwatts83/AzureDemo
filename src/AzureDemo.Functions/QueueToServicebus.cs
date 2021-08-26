6967c44ee5c18c83ccd4937392188363c55f6148
Alex Watts <alexjwatts83@gmail.com>
2021-08-27 07:39:56 +1000

    Made QueueToServicebus non static to DI

--- src/AzureDemo.Functions/QueueToServicebus.cs
+++ src/AzureDemo.Functions/QueueToServicebus.cs
@@ -1,13 +1,18 @@
 using System;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace AzureDemo.Functions
 {
-	public static class QueueToServicebus
+	public class QueueToServicebus
     {
+		public QueueToServicebus(IConfiguration config)
+		{
+
+		}
         [FunctionName("QueueToServicebus")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
