761c365b101065ffcf987230800ab807f2a148a8
Alex Watts <alexjwatts83@gmail.com>
2021-08-27 07:45:24 +1000

    made TriggerToServicebus async and not static

--- src/AzureDemo.Functions/TriggerToServicebus.cs
+++ src/AzureDemo.Functions/TriggerToServicebus.cs
@@ -1,12 +1,17 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace AzureDemo.Functions
 {
-	public static class TriggerToServicebus
+	public class TriggerToServicebus
     {
+		public TriggerToServicebus()
+		{
+
+		}
         [FunctionName("TriggerToServicebus")]
-        public static void Run([ServiceBusTrigger("test-queue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        public async Task Run([ServiceBusTrigger("test-queue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
