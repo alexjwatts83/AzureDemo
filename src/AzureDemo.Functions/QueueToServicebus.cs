e29f2131ba2aee5fe7b3dd40ddc245bfa90116c6
Alex Watts <alexjwatts83@gmail.com>
2021-08-27 07:43:25 +1000

    Made QueueToServicebus to async

--- src/AzureDemo.Functions/QueueToServicebus.cs
+++ src/AzureDemo.Functions/QueueToServicebus.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -7,14 +9,29 @@ namespace AzureDemo.Functions
 {
 	public class QueueToServicebus
     {
+		private readonly string _serviceBusConnectionString;
+
 		public QueueToServicebus(IConfiguration config)
 		{
-
+			_serviceBusConnectionString = config.GetConnectionString("ServiceBus");
 		}
+
         [FunctionName("QueueToServicebus")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-        }
+
+			log.LogInformation($"Sending videos to queue at: {DateTime.Now}");
+			await using (var client = new ServiceBusClient(_serviceBusConnectionString))
+			{
+				var sender = client.CreateSender("new-youtubevideo");
+				//foreach (var videoId in videos)
+				//{
+				//	await sender.SendMessageAsync(new ServiceBusMessage(videoId));
+				//	await _videoService.SaveYoutubeVideo(videoId);
+				//	log.LogInformation($"Queued: {videoId}");
+				//}
+			}
+		}
     }
 }
